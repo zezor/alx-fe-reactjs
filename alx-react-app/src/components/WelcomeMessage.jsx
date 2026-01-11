833d49a0f7efad891c9c6ab43f35ab6491a1771b
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-01-11 00:11:24 +0000

    setting up react app

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;

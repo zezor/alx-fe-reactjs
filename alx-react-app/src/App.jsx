cef593f3458eb320bf101f27df3b84b2d3e77598
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-01-11 00:27:53 +0000

    moving Header.jsx, FOoter.jsx into the components folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)

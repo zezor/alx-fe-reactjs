a43ceb20a5b16625a03ed16e85b07e7b4df9adf3
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-01-11 00:54:16 +0000

    fix: correct spacing in UserProfile props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name = "Alice" age = {25} bio = "Loves hiking and photography" />
+       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <div> 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

95ed5d16ffa89d9a15d4f32210b030859f13b23d
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-01-11 00:37:32 +0000

    correcting props

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,20 @@
-function UserProfile ({ name, age, bio }) {
+// function UserProfile ({ name, age, bio }) {
+//     return (
+//         <div>
+//             <h2>{name}</h2>
+//             <p>Age: {age}</p>
+//             <p>Bio: {bio}</p>
+//         </div>
+//     );
+// }
+// export default UserProfile;
+
+function UserProfile ({ props }) {
     return (
         <div>
-            <h2>{name}</h2>
-            <p>Age: {age}</p>
-            <p>Bio: {bio}</p>
+            <h2>{props.name}</h2>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
         </div>
     );
 }

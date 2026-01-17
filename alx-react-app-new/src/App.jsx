2c6fab8d0c1b35b393b41137114b5b0ca65503d7
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-01-17 23:01:48 +0000

    counter with useState

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +19,7 @@ function App() {
       <Footer />
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       
-      
+      <Counter/>
       <WelcomeMessage /> 
     </>
   )

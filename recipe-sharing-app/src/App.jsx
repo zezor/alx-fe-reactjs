5b03a88fddcaaff9a808023dfc4a291e49eb59eb
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-01-25 00:57:11 +0000

    router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';

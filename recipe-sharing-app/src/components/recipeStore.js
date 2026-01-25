be293aad9e860e4009b71ae3938790630c380f13
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-01-25 00:25:20 +0000

    moving recipe recipeStore.js

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
+}));

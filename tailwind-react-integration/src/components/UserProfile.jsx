41aa8deed7cd734608bb277d7118b97312791ea0
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-07 17:24:15 +0000

    making a responsive styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full mx-auto w-36 h-36"/>
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg text-center">
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full m-4 mx-auto md:w-36 h-36 sm:w-24 sm:h-24"/>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }

3d271aab7c5371a1833fc884813ff432a4ae8a5b
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-22 19:29:15 +0000

    form

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from './components/RegistrationForm'
-import FormikForm from './components/FormikForm.jsx'
+import FormikForm from './components/formikForm.js'
 
 function App() {
   const [count, setCount] = useState(0)
